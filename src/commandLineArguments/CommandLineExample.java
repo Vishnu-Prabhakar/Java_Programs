fbfcfc7e4318fcff62fcbf02b6be3105003a4cc7
ENTERPRISE\ravi9001 <vishnu.ragupathy@nielseniq.com>
2021-03-24 12:56:00 +0530

    Added command line arguments example

---
+++ src/commandLineArguments/CommandLineExample.java
@@ -0,0 +1,11 @@
+package commandLineArguments;
+
+public class CommandLineExample {
+	public static void main(String arg121w121s[]) {
+		System.out.println("Your first argument is: " + arg121w121s[0] +" "+ arg121w121s[1]);
+	}
+	
+	public static void main(String test){
+		System.out.println(test);
+	}
+}
