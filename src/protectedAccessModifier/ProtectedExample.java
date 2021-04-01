7829e12aeae79ff9319c34a843718b3fc6953214
ENTERPRISE\ravi9001 <vishnu.ragupathy@nielseniq.com>
2021-04-01 23:00:50 +0530

    Added protected access modifier example

---
+++ src/protectedAccessModifier/ProtectedExample.java
@@ -0,0 +1,9 @@
+package protectedAccessModifier;
+import accessModifiers.*;
+
+class ProtectedExample extends A2 {
+	public static void main(String args[]) {
+		ProtectedExample obj = new ProtectedExample();
+		obj.msg();
+	}
+}
